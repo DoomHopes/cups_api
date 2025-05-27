50fbdee71bc64ea486d05995187458b72a5a1a50
Alex UD <alex.ud@webhive.com.ua>
2025-05-27 18:49:11 +0300

    first commit

---
+++ routes/hello.dart
@@ -0,0 +1,15 @@
+import 'package:dart_frog/dart_frog.dart';
+
+Response onRequest(RequestContext context) {
+  
+  if (context.request.method == HttpMethod.get) {
+    return Response.json(
+      body: {'message': 'Hello'},
+    );
+  }
+
+  return Response(
+    statusCode: 405,
+    body: 'Method Not Allowed',
+  );
+}
